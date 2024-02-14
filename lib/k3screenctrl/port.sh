152efd4e6fc39b597ab02e099f1426065f51c4ad
张明达 <30994124+MingDa9916@users.noreply.github.com>
2024-02-14 17:25:11 +0800

    Update port.sh

--- lib/k3screenctrl/port.sh
+++ lib/k3screenctrl/port.sh
@@ -2,9 +2,9 @@
 
 print_eth_port_status() {
     local port=$1
-
+    
     # One `swconfig dev switch0 show` wastes more time than 4 `port show`
-    if [ -n "`swconfig dev switch0 port $port show | grep \"link:up\"`" ]; then
+    if [ -n "`ip link show | grep $port@eth0 | grep 'state UP'`" ]; then
         echo 1
     else
         echo 0
@@ -19,8 +19,8 @@ print_usb_port_status() {
     fi
 }
 
-print_eth_port_status 1 # Port 1 is LAN1 on label
-print_eth_port_status 0 # Port 0 is LAN2 on label
-print_eth_port_status 2 # LAN3
-print_eth_port_status 3 # WAN
+print_eth_port_status lan2 # lan2@eth0 is LAN1 on label
+print_eth_port_status lan1 # lan1@eth0 is LAN2 on label
+print_eth_port_status lan3 # lan3@eth0 is LAN3 on label
+print_eth_port_status wan # WAN
 print_usb_port_status
